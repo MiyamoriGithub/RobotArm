dbebdfc014daa29af53cbf97dc876fc67e3b39a5
Daniel <chenyangvoice@gmial.com>
2021-10-15 22:53:18 +0800

    优化了代码注释，精简部分代码，提高代码的可读性和运行效率

--- mulit.c
+++ mulit.c
@@ -3,7 +3,7 @@
 #define uchar unsigned char
 #define uint unsigned int
 
-// 单个动作结构体数组
+// 定义舵机动作结构体
 struct Motion {
 	// 舵机识别号，1-4表示4个舵机，0为无效
 	uchar iden;
@@ -15,12 +15,13 @@ struct Motion {
 #define TIME 5000
 
 // 定义舵机动作组，初值根据实际情况修改
-struct Motion motion0[10]={{1, 200}, {2, 210}, {3, 200}, {4, 185}};
-struct Motion motion1[10]={{1, 100}, {2, 120}, {3, 110}, {4, 100}};
+struct Motion motion0[10]={{1, 200}, {2, 210}, {3, 200}, {4, 200}};
+struct Motion motion1[10]={{1, 135}, {2, 135}, {3, 135}, {4, 135}};
+// mo0 和 mo1 为动作组记忆计数
 uchar mo0=0;
 uchar mo1=0;
 
-// 定义舵机初始位置，当前为中位
+// 定义舵机位置，当前初值为舵机中位
 uint servo1=135;
 uint servo2=135;
 uint servo3=135;
@@ -33,12 +34,10 @@ sbit PWM3=P3^2;
 sbit PWM4=P3^3;
 
 // 定义LED状态指示灯信号输出
-// sbit LED0=P0^0;
 sbit LED1=P0^1;
 sbit LED2=P0^2;
 
-// key0-key7定义舵机控制按键信号
-// 两个一组控制舵机位置加减
+// key0-key7定义舵机控制按键信号，两个一组控制舵机位置加减
 sbit key0=P2^0;
 sbit key1=P2^1;
 sbit key2=P2^2;
@@ -48,7 +47,7 @@ sbit key5=P2^5;
 sbit key6=P2^6;
 sbit key7=P2^7;
 // but0-but3定义功能按键信号
-// 0、1为开启动作组按键，2、3为动作组记忆使能按键
+// 0、1为开启动作组按键，2、3为动作组记忆状态按键
 sbit but0=P1^0;
 sbit but1=P1^1;
 sbit but2=P1^2;
@@ -59,12 +58,10 @@ uchar flag0=0;
 uchar flag1=0;
 uchar flag2=0;
 uchar flag3=0;
-uchar state2=0;
-uchar state3=0;
 uchar flag0f=0;
 uchar flag1f=0;
-uchar flag2f=0;
-uchar flag3f=0;
+uchar state2=0;
+uchar state3=0;
 
 // 舵机控制按键标志位
 uchar turn_flag=0;
@@ -127,9 +124,8 @@ void turn(uchar iden, int target) {
 	}
 }
 
-// 转动函数，iden为舵机识别号
-// 按键点动控制舵机时调用
-// 225为舵机占空比的数值上限，调整可以限制舵机转动角度
+// 转动函数，iden为按键识别号。按键点动控制舵机时调用
+// 225为舵机占空比的数值上限，42为舵机占空比的数值下限，调整可以限制舵机转动角度
 void turn_inf(uchar iden) {
 	switch (iden) {
 		case 1:
@@ -139,97 +135,75 @@ void turn_inf(uchar iden) {
 			}
 			break;
 		case 2:
-			while(turn_flag>0 && servo2 < 225) {
-				servo2++;
-				delay1();
-			}
-			break;
-		case 3:
-			while(turn_flag>0 && servo3 < 225) {
-				servo3++;
-				delay1();
-			}
-			break;
-		case 4:
-			while(turn_flag>0 && servo4 < 225) {
-				servo4++;
-				delay1();
-			}
-			break;
-	}
-	if(state2==1 && mo0 <10) {
-		motion0[mo0].iden=iden;
-		switch (iden) {
-			case 1: motion0[mo0].motion=servo1; break;
-			case 2: motion0[mo0].motion=servo2; break;
-			case 3: motion0[mo0].motion=servo3; break;
-			case 4: motion0[mo0].motion=servo4; break;
-		}
-		++mo0;
-	}
-	if(state3==1 && mo1 <10) {
-		motion1[mo1].iden=iden;
-		switch (iden) {
-			case 1: motion1[mo1].motion=servo1; break;
-			case 2: motion1[mo1].motion=servo2; break;
-			case 3: motion1[mo1].motion=servo3; break;
-			case 4: motion1[mo1].motion=servo4; break;
-		}
-		++mo1;
-	}
-}
-
-// 反向转动函数，iden为舵机识别号
-// 按键点动控制舵机时调用
-// 42为舵机占空比的数值下限，调整可以限制舵机转动角度
-void reverse_inf(uchar iden) {
-	switch (iden) {
-		case 1:
 			while(turn_flag>0 && servo1 > 42) {
 				servo1--;
 				delay1();
 			}
 			break;
-		case 2:
+		case 3:
+			while(turn_flag>0 && servo2 < 225) {
+				servo2++;
+				delay1();
+			}
+			break;
+		case 4:
 			while(turn_flag>0 && servo2 > 42) {
 				servo2--;
 				delay1();
 			}
 			break;
-		case 3:
+		case 5:
+			while(turn_flag>0 && servo3 < 225) {
+				servo3++;
+				delay1();
+			}
+			break;
+		case 6:
 			while(turn_flag>0 && servo3 > 42) {
 				servo3--;
 				delay1();
 			}
 			break;
-		case 4:
+		case 7:
+			while(turn_flag>0 && servo4 < 225) {
+				servo4++;
+				delay1();
+			}
+			break;
+		case 8:
 			while(turn_flag>0 && servo4 > 42) {
 				servo4--;
 				delay1();
 			}
 			break;
 	}
+	// 实现记忆功能，当标志位打开且动作组计数小于10，则将当前的舵机识别号和舵机状态存入动作组
 	if(state2==1 && mo0 <10) {
-		motion0[mo0].iden=iden;
 		switch (iden) {
-			case 1: motion0[mo0].motion=servo1; break;
-			case 2: motion0[mo0].motion=servo2; break;
-			case 3: motion0[mo0].motion=servo3; break;
-			case 4: motion0[mo0].motion=servo4; break;
+			case 1: motion0[mo0].iden=1; motion0[mo0].motion=servo1; break;
+			case 2: motion0[mo0].iden=1; motion0[mo0].motion=servo1; break;
+			case 3: motion0[mo0].iden=2; motion0[mo0].motion=servo2; break;
+			case 4: motion0[mo0].iden=2; motion0[mo0].motion=servo2; break;
+			case 5: motion0[mo0].iden=3; motion0[mo0].motion=servo3; break;
+			case 6: motion0[mo0].iden=3; motion0[mo0].motion=servo3; break;
+			case 7: motion0[mo0].iden=4; motion0[mo0].motion=servo4; break;
+			case 8: motion0[mo0].iden=4; motion0[mo0].motion=servo4; break;
 		}
 		++mo0;
 	}
 	if(state3==1 && mo1 <10) {
-		motion1[mo1].iden=iden;
 		switch (iden) {
-			case 1: motion1[mo1].motion=servo1; break;
-			case 2: motion1[mo1].motion=servo2; break;
-			case 3: motion1[mo1].motion=servo3; break;
-			case 4: motion1[mo1].motion=servo4; break;
+			case 1: motion0[mo1].iden=1; motion1[mo1].motion=servo1; break;
+			case 2: motion0[mo0].iden=1; motion1[mo1].motion=servo1; break;
+			case 3: motion0[mo0].iden=2; motion1[mo1].motion=servo2; break;
+			case 4: motion0[mo0].iden=2; motion1[mo1].motion=servo2; break;
+			case 5: motion0[mo0].iden=3; motion1[mo1].motion=servo3; break;
+			case 6: motion0[mo0].iden=3; motion1[mo1].motion=servo3; break;
+			case 7: motion0[mo0].iden=4; motion1[mo1].motion=servo4; break;
+			case 8: motion0[mo0].iden=4; motion1[mo1].motion=servo4; break;
 		}
 		++mo1;
 	}
-
 }
 
 // 计时器T0初始化
@@ -237,7 +211,6 @@ void reverse_inf(uchar iden) {
 void timer_int() {
 	// 开启T0的16位工作模式
 	TMOD = 0x11;
-	// 装入计时高8位和低8位
 	TH0=(65536-TIME)/256;
 	TL0=(65536-TIME)%256;
 	// 开启定时器T0的溢出中断
@@ -252,7 +225,9 @@ void timer_int() {
 
 // 按键扫描函数
 void get_key() {
-	// 点动控制，不作消抖处理
+	// 两个按键为一组控制舵机的正转和反转。点动控制，不作消抖处理
+	// 按键按下后，turn_flag 标志位置于对应状态，按键抬起后，turn_flag 标志位归零
+
 	// 控制舵机1
 	if(key0==0)
 		turn_flag=1;
@@ -307,33 +282,37 @@ void get_key() {
 		flag1=0;
 		flag1f=1;
 	}
-	// 动作组 0 的记忆开关
+	// 动作组 0 的记忆按键，两个记忆按键是互锁关系
 	if(but2==0&&flag2==0) {
 		flag2=1;
 	}
 	if(but2==1&&flag2==1) {
 		flag2=0;
+		// 按键被按下后，在开关两个状态之间切换，状态通过 LED1 输出
 		if(state2==0 && state3==0) {
 			state2=1;
 			LED1=0;
 		} else {
 			state2=0;
 			LED1=1;
+			// 记忆功能关闭，动作组计数归零
 			mo0==0;
 		}
 	}
-	// 动作组 1 的记忆开关
+	// 动作组 1 的记忆按键，两个记忆按键是互锁关系
 	if(but3==0&&flag3==0) {
 		flag3=1;
 	}
 	if(but3==1&&flag3==1) {
 		flag3=0;
+		// 按键被按下后，在开关两个状态之间切换，状态通过 LED2 输出
 		if(state3==0 && state2==0) {
 			state3=1;
 			LED2=0;
 		} else {
 			state3=0;
 			LED2=1;
+			// 记忆功能关闭，动作组计数归零
 			mo1==0;
 		}
 	}
@@ -375,23 +354,15 @@ void timer() interrupt 1 {
 void main() {
     timer_int();
     while(1) {
-		// 按键控制转动
+		// 按键控制舵机转动
 		if(turn_flag>0) {
-			switch (turn_flag) {
-				case 1: turn_inf(1); break;
-				case 2: reverse_inf(1); break;
-				case 3: turn_inf(2); break;
-				case 4: reverse_inf(2); break;
-				case 5: turn_inf(3); break;
-				case 6: reverse_inf(3); break;
-				case 7: turn_inf(4); break;
-				case 8: reverse_inf(4); break;
-			}
+			turn_inf(turn_flag);
 		}
-		// 动作组
+		// 如果 but0 被按下，按照先后顺序执行存储在 motion0 中的动作组
 		if(flag0f==1) {
 			int ff;
 			for(ff=0; ff<10; ff++) {
+				// 如果识别码为无效，则之后无可执行动作，终止循环
 				if(motion0[ff].iden==0) {
 					break;
 				}
@@ -401,9 +372,11 @@ void main() {
 			}
 			flag0f=0;
 		}
+		// 如果 but1 被按下，按照先后顺序执行存储在 motion1 中的动作组
 		if(flag1f==1) {
 			int gg;
 			for(gg=0; gg<10; gg++) {
+				// 如果识别码为无效，则之后无可执行动作，终止循环
 				if(motion1[gg].iden==0) {
 					break;
 				}
